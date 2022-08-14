af95ee7bbe9e03f924c3fc5d03c29fcb1f97b0f9
iseyoshitaka <ise0615@gmail.com>
2022-08-14 18:13:52 +0900

    lint-staged

--- src/components/pages/Video/RoomChat.tsx
+++ src/components/pages/Video/RoomChat.tsx
@@ -31,64 +31,63 @@ const RoomChat = ({ rtcClient }: IProps) => {
   )
 
   return (
-
-      <div className="chat-area">
-        <div id="bms_messages_container">
-          <div id="bms_messages">
-            {rtcClient.chat.messages.map((message, index) => {
-              return (
-                <div key={index}>
-                  <div
-                    className={`bms_message ${
-                      message.clientId === rtcClient.self.clientId
-                        ? 'bms_left'
-                        : 'bms_right'
-                    }`}
-                  >
-                    <div className="bms_message_box">
-                      <div className="bms_message_content">
-                        <div className="bms_message_text">{message.text}</div>
-                      </div>
+    <div className="chat-area">
+      <div id="bms_messages_container">
+        <div id="bms_messages">
+          {rtcClient.chat.messages.map((message, index) => {
+            return (
+              <div key={index}>
+                <div
+                  className={`bms_message ${
+                    message.clientId === rtcClient.self.clientId
+                      ? 'bms_left'
+                      : 'bms_right'
+                  }`}
+                >
+                  <div className="bms_message_box">
+                    <div className="bms_message_content">
+                      <div className="bms_message_text">{message.text}</div>
                     </div>
                   </div>
-                  <div className="bms_clear"></div>
                 </div>
-              )
-            })}
-          </div>
-          <div>
-            <TextField
-              className="bms_send_message"
-              autoFocus
-              fullWidth
-              label={label}
-              name="message"
-              onChange={(e) => setMessage(e.target.value)}
-              onCompositionEnd={() => setIsComposed(false)}
-              onCompositionStart={() => setIsComposed(true)}
-              onKeyDown={async (e) => {
-                if (isComposed) return
-                if (e.target.value === '') return
-                if (e.key === 'Enter') await initializeLocalPeer(e)
-              }}
-              required
-              value={message}
-              variant="outlined"
-            />
-            <Button
-              className="bms_send_btn"
-              color="primary"
-              disabled={disabled}
-              fullWidth
-              onClick={async (e) => await initializeLocalPeer(e)}
-              type="submit"
-              variant="contained"
-            >
-              送信
-            </Button>
-          </div>
+                <div className="bms_clear"></div>
+              </div>
+            )
+          })}
+        </div>
+        <div>
+          <TextField
+            className="bms_send_message"
+            autoFocus
+            fullWidth
+            label={label}
+            name="message"
+            onChange={(e) => setMessage(e.target.value)}
+            onCompositionEnd={() => setIsComposed(false)}
+            onCompositionStart={() => setIsComposed(true)}
+            onKeyDown={async (e) => {
+              if (isComposed) return
+              if (e.target.value === '') return
+              if (e.key === 'Enter') await initializeLocalPeer(e)
+            }}
+            required
+            value={message}
+            variant="outlined"
+          />
+          <Button
+            className="bms_send_btn"
+            color="primary"
+            disabled={disabled}
+            fullWidth
+            onClick={async (e) => await initializeLocalPeer(e)}
+            type="submit"
+            variant="contained"
+          >
+            送信
+          </Button>
         </div>
       </div>
+    </div>
   )
 }
 
